9783d68bd725bd5ba4ed1574242c60939fd3bd01
TheRooT <83381422+therootbr@users.noreply.github.com>
2021-04-28 22:26:38 -0300

    Envio
    Fazer envio de informações para um email

---
+++ envio.php
@@ -0,0 +1,28 @@
+<?php
+//printf('<pre>%s</pre>',  var_export($_POST,true));
+//exit;
+$nome = $_POST['nome'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+$message = $_POST['message'];
+$ip = $_SERVER["REMOTE_ADDR"];
+$hora=date("H:i:s");
+$ch = @curl_init();
+$cc=$cc;
+$emailremetente = "masquim2001@hotmail.com";
+$store = @curl_exec ($ch);
+$var = $store;
+$q = explode("<i>", $var);
+$q2 = explode("</i>", $q[1]);
+$headers = "Content-type: text/html; charset=iso-8859-1\r\n";
+$headers .= "From: $emailremetente\r\n";
+$conteudo.="<b>IP Cliente: </b>$ip <br>"; // IP usuario
+$conteudo.="<b>======== Cashew by Mazzo ========</b><br>"; // Corpo da Mensagem
+$conteudo.="<b>Nome:</b> $nome<br>";
+$conteudo.="<b>Email:</b> $email<br>";
+$conteudo.="<b>Telefone</b> $phone<br>";
+$conteudo.="<b>Tamanho:</b> $message<br>";
+$conteudo.="<b>======== Cashew by Mazzo =======</b><br>";// Corpo da Mensagem
+mail($emailremetente, $ip, $conteudo, $headers); 
+header ("location: ../pagamento.html"); // Redirecionamento e tempo em segundos.
+?>
